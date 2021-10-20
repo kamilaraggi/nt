86dc45533e00ebe2e476479b61728f686b976ce6
Kamila Raggi <kamilaraggi@hotmail.com>
2021-10-20 14:08:29 -0400

    api routes

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 const express = require('express');
 const fs = require('fs');
-const app = express();
 const path = require ('path');
 const notes = require('./db/db.json');
-var PORT = process.env.PORT || 8001;
+const index = require ('./public/index.html');
+const notesHtml = require('./public/notes.html')
+
+const app = express();
+var PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
 app.use(express.json());
+app.use(express.static("./public"));
+app.use("./api/notes", notes);
+app.use("/", index);
+app.use("/notes", notesHtml);
 
-app.get('/api/notes'), (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'));
+app.get("/api/notes"), (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 };
+
+app.post("/api/notes", (req, res) => {
+const notes = req.body;
+});
+// HTML\\
+app.get("/notes"), (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+};
+
+app.get("/"), function (req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+};
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
