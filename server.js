cc7d58aecbbcb0b0ca1b5027f5b8b3485f4001b9
Kamila Raggi <kamilaraggi@hotmail.com>
2021-10-20 12:11:36 -0400

    route to notes

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 const app = express();
 const path = require ('path');
 const notes = require('./db/db.json');
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 8001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 
-app.get('/api/db', (req, res) => {
-  res.json(notes);
-});
+app.get('/api/notes'), (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
+};
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
